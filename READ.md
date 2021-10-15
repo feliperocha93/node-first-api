2da726a469f54b1a68a24d8cab424f4198fd31ee
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-15 13:43:49 -0300

    it was created the logic to find user by id

---
+++ READ.md
@@ -0,0 +1,16 @@
+### Draft
+READ.me will be revised and finalized when I finish the course module.
+
+This project is a node API made without using the Express framework.
+
+Using the own node modules and simple js logics, it was created a http server,
+with a routing system that respect the REST best practices
+
+The project was done in first module of (JStack)[LINK DO CURSO] course, "First steps in Node.js",
+
+What I learned from this module
+- how to work with modules
+- how to create http server
+- the logic behind routing system
+- how is get the query params in url
+- how is get the path params in url and pass to route
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { URL } = require('url');
+const { filter } = require('./routes');
 
 const routes = require('./routes');
 
@@ -9,12 +10,24 @@ const server = http.createServer((request, response) => {
 
   console.log(`Request method: ${method} | Endpoint: ${parsedUrl.pathname}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === method
+    routeObj.endpoint === pathname && routeObj.method === method
   ));
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+    
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
