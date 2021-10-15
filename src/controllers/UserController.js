e2fcc28091828e3f4e967646d3371dcf7f331fd6
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-15 14:26:19 -0300

    refactoring: the send method was created in the routes response

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -12,8 +12,7 @@ module.exports = {
       return a.id > b.id ? 1 : -1;
     });
 
-    response.writeHead(200, { 'Content-Type': 'application/json' } );
-    response.end(JSON.stringify(sortedUsers));
+    response.send(200, sortedUsers);
   },
 
   getUserById(request, response) {
@@ -22,12 +21,9 @@ module.exports = {
     const user = users.find((user) => user.id === Number(id));
 
     if (!user) {
-      response.writeHead(200, { 'Content-Type': 'application/json' } );
-      response.end(JSON.stringify({ error: 'User not found'} ));
-    } else {
-      response.writeHead(200, { 'Content-Type': 'application/json' } );
-      response.end(JSON.stringify(user));
+      return response.send(400, { error: 'User not found' });
     }
 
+    response.send(200, user);
   },
 };
--- src/index.js
+++ src/index.js
@@ -27,6 +27,11 @@ const server = http.createServer((request, response) => {
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
     request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    }
     
     route.handler(request, response);
   } else {
